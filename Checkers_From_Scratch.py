a9cec4b658349a5ee768a8968702fc2b17bb8405
Levi J. Masters <42942430+Levijom@users.noreply.github.com>
2018-09-13 11:22:42 -0400

    Copied someone else's board
    https://github.com/everestwitman/Pygame-Checkers/blob/master/checkers.py
    Copied code from the above link.  Trying to create my own version by understanding this one.

--- Checkers_From_Scratch.py
+++ Checkers_From_Scratch.py
@@ -1,25 +1,210 @@
 
-import tkinter as tk
+#code from https://github.com/everestwitman/Pygame-Checkers/blob/master/checkers.py
 
-window = tk.Tk()
+class Board:
+	def __init__(self):
+		self.matrix = self.new_board()
 
-window.title("Checkers")
+	def new_board(self):
+		"""
+		Create a new board matrix.
+		"""
 
-window.geometry("400x400")
+		# initialize squares and place them in matrix
 
-# LABEL
-title = tk.Label(text="Hello world")
-title.grid(column=0, row=0)
+		matrix = [[None] * 8 for i in xrange(8)]
 
-# Button
-button1 = tk.Button(text="Click me!")
-button1.grid(column=0, row=1)
+		# The following code block has been adapted from
+		# http://itgirl.dreamhosters.com/itgirlgames/games/Program%20Leaders/ClareR/Checkers/checkers.py
+		for x in xrange(8):
+			for y in xrange(8):
+				if (x % 2 != 0) and (y % 2 == 0):
+					matrix[y][x] = Square(WHITE)
+				elif (x % 2 != 0) and (y % 2 != 0):
+					matrix[y][x] = Square(BLACK)
+				elif (x % 2 == 0) and (y % 2 != 0):
+					matrix[y][x] = Square(WHITE)
+				elif (x % 2 == 0) and (y % 2 == 0): 
+					matrix[y][x] = Square(BLACK)
+
+		# initialize the pieces and put them in the appropriate squares
+
+		for x in xrange(8):
+			for y in xrange(3):
+				if matrix[x][y].color == BLACK:
+					matrix[x][y].occupant = Piece(RED)
+			for y in xrange(5, 8):
+				if matrix[x][y].color == BLACK:
+					matrix[x][y].occupant = Piece(BLUE)
+
+		return matrix
+
+	def board_string(self, board):
+		"""
+		Takes a board and returns a matrix of the board space colors. Used for testing new_board()
+		"""
+
+		board_string = [[None] * 8] * 8 
+
+		for x in xrange(8):
+			for y in xrange(8):
+				if board[x][y].color == WHITE:
+					board_string[x][y] = "WHITE"
+				else:
+					board_string[x][y] = "BLACK"
 
 
-# Entry field
-entry_field1 = tk.Entry()
-entry_field1.grid(column=0, row=2)
+		return board_string
+	
+	def rel(self, dir, (x,y)):
+		"""
+		Returns the coordinates one square in a different direction to (x,y).
+		===DOCTESTS===
+		>>> board = Board()
+		>>> board.rel(NORTHWEST, (1,2))
+		(0,1)
+		>>> board.rel(SOUTHEAST, (3,4))
+		(4,5)
+		>>> board.rel(NORTHEAST, (3,6))
+		(4,5)
+		>>> board.rel(SOUTHWEST, (2,5))
+		(1,6)
+		"""
+		if dir == NORTHWEST:
+			return (x - 1, y - 1)
+		elif dir == NORTHEAST:
+			return (x + 1, y - 1)
+		elif dir == SOUTHWEST:
+			return (x - 1, y + 1)
+		elif dir == SOUTHEAST:
+			return (x + 1, y + 1)
+		else:
+			return 0
+
+	def adjacent(self, (x,y)):
+		"""
+		Returns a list of squares locations that are adjacent (on a diagonal) to (x,y).
+		"""
+
+		return [self.rel(NORTHWEST, (x,y)), self.rel(NORTHEAST, (x,y)),self.rel(SOUTHWEST, (x,y)),self.rel(SOUTHEAST, (x,y))]
+
+	def location(self, (x,y)):
+		"""
+		Takes a set of coordinates as arguments and returns self.matrix[x][y]
+		This can be faster than writing something like self.matrix[coords[0]][coords[1]]
+		"""
+
+		return self.matrix[x][y]
+
+	def blind_legal_moves(self, (x,y)):
+		"""
+		Returns a list of blind legal move locations from a set of coordinates (x,y) on the board. 
+		If that location is empty, then blind_legal_moves() return an empty list.
+		"""
+
+		if self.matrix[x][y].occupant != None:
+			
+			if self.matrix[x][y].occupant.king == False and self.matrix[x][y].occupant.color == BLUE:
+				blind_legal_moves = [self.rel(NORTHWEST, (x,y)), self.rel(NORTHEAST, (x,y))]
+				
+			elif self.matrix[x][y].occupant.king == False and self.matrix[x][y].occupant.color == RED:
+				blind_legal_moves = [self.rel(SOUTHWEST, (x,y)), self.rel(SOUTHEAST, (x,y))]
+
+			else:
+				blind_legal_moves = [self.rel(NORTHWEST, (x,y)), self.rel(NORTHEAST, (x,y)), self.rel(SOUTHWEST, (x,y)), self.rel(SOUTHEAST, (x,y))]
+
+		else:
+			blind_legal_moves = []
+
+		return blind_legal_moves
+
+	def legal_moves(self, (x,y), hop = False):
+		"""
+		Returns a list of legal move locations from a given set of coordinates (x,y) on the board.
+		If that location is empty, then legal_moves() returns an empty list.
+		"""
+
+		blind_legal_moves = self.blind_legal_moves((x,y)) 
+		legal_moves = []
+
+		if hop == False:
+			for move in blind_legal_moves:
+				if hop == False:
+					if self.on_board(move):
+						if self.location(move).occupant == None:
+							legal_moves.append(move)
+
+						elif self.location(move).occupant.color != self.location((x,y)).occupant.color and self.on_board((move[0] + (move[0] - x), move[1] + (move[1] - y))) and self.location((move[0] + (move[0] - x), move[1] + (move[1] - y))).occupant == None: # is this location filled by an enemy piece?
+							legal_moves.append((move[0] + (move[0] - x), move[1] + (move[1] - y)))
+
+		else: # hop == True
+			for move in blind_legal_moves:
+				if self.on_board(move) and self.location(move).occupant != None:
+					if self.location(move).occupant.color != self.location((x,y)).occupant.color and self.on_board((move[0] + (move[0] - x), move[1] + (move[1] - y))) and self.location((move[0] + (move[0] - x), move[1] + (move[1] - y))).occupant == None: # is this location filled by an enemy piece?
+						legal_moves.append((move[0] + (move[0] - x), move[1] + (move[1] - y)))
+
+		return legal_moves
+
+	def remove_piece(self, (x,y)):
+		"""
+		Removes a piece from the board at position (x,y). 
+		"""
+		self.matrix[x][y].occupant = None
+
+	def move_piece(self, (start_x, start_y), (end_x, end_y)):
+		"""
+		Move a piece from (start_x, start_y) to (end_x, end_y).
+		"""
+
+		self.matrix[end_x][end_y].occupant = self.matrix[start_x][start_y].occupant
+		self.remove_piece((start_x, start_y))
+
+		self.king((end_x, end_y))
+
+	def is_end_square(self, coords):
+		"""
+		Is passed a coordinate tuple (x,y), and returns true or 
+		false depending on if that square on the board is an end square.
+		===DOCTESTS===
+		>>> board = Board()
+		>>> board.is_end_square((2,7))
+		True
+		>>> board.is_end_square((5,0))
+		True
+		>>>board.is_end_square((0,5))
+		False
+		"""
+
+		if coords[1] == 0 or coords[1] == 7:
+			return True
+		else:
+			return False
+
+	def on_board(self, (x,y)):
+		"""
+		Checks to see if the given square (x,y) lies on the board.
+		If it does, then on_board() return True. Otherwise it returns false.
+		===DOCTESTS===
+		>>> board = Board()
+		>>> board.on_board((5,0)):
+		True
+		>>> board.on_board(-2, 0):
+		False
+		>>> board.on_board(3, 9):
+		False
+		"""
+
+		if x < 0 or y < 0 or x > 7 or y > 7:
+			return False
+		else:
+			return True
 
 
-window.mainloop()
-
+	def king(self, (x,y)):
+		"""
+		Takes in (x,y), the coordinates of square to be considered for kinging.
+		If it meets the criteria, then king() kings the piece in that square and kings it.
+		"""
+		if self.location((x,y)).occupant != None:
+			if (self.location((x,y)).occupant.color == BLUE and y == 0) or (self.location((x,y)).occupant.color == RED and y == 7):
+				self.location((x,y)).occupant.king = True 
